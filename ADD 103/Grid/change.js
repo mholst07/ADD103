bbc07961055bb9d2da15e22a504c52a6250e192e
mholst07 <mholst@students.mchenry.edu>
2025-10-26 13:34:48 -0500

    Add files via upload

---
+++ ADD 103/Grid/change.js
@@ -0,0 +1,23 @@
+function change01(){
+    document.getElementById("large").src="images/acorn_resized.jpg";
+}
+
+function change02(){
+    document.getElementById("large").src="images/eagles_cropped.jpg";
+}
+
+function change03(){
+    document.getElementById("large").src="images/fish_flipped.jpg";
+}
+
+function change04(){
+    document.getElementById("large").src="images/forest_preserve_compressed.jpg";
+}
+
+function change05(){
+    document.getElementById("large").src="images/lake_exposured.jpg";
+}
+
+function change06(){
+    document.getElementById("large").src="images/ocean_rotated.jpg";
+}
